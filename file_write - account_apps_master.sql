995f25f10867126618629fd9bd640780c1a227f6
awaisraza7852 <51000185+awaisraza7852@users.noreply.github.com>
2023-08-25 11:04:20 +0500

    Update file_write - account_apps_master.sql

--- file_write - account_apps_master.sql
+++ file_write - account_apps_master.sql
@@ -2,22 +2,21 @@ CREATE OR REPLACE PROCEDURE FLEXCUBE3.file_write_loan_details
 AS
    CURSOR c_data
    IS
-      select account_number loan_number,
-      amount_financed loan_amount,
-      book_date,
-      maturity_date,
-      dr_prOd_ac customer_number
-      from flexcube3.cltb_account_apps_master
-      where extract(year from VALUE_DATE) = '2019'
-      and product_code = 'CDCF';
+      select column1,
+      column2,
+      column3,
+      column4,
+      column5
+      from table_name
+      where <condition>;
 
 
 
    v_file   UTL_FILE.FILE_TYPE;
 BEGIN
    v_file :=
-      UTL_FILE.FOPEN (location       => '/u01/',
-                      filename       => 'loan_details.csv',
+      UTL_FILE.FOPEN (location       => '<location>',
+                      filename       => '<file_name>',
                       open_mode      => 'w',
                       max_linesize   => 32767);
 
@@ -25,15 +24,15 @@ BEGIN
    LOOP
       UTL_FILE.PUT_LINE (
          v_file,
-            cur_rec.LOAN_NUMBER
+            cur_rec.column1
          || ','
-         || cur_rec.LOAN_AMOUNT
+         || cur_rec.column2
          || ','
-         || cur_rec.BOOK_DATE
+         || cur_rec.column3
          || ','
-         || cur_rec.MATURITY_DATE
+         || cur_rec.column4
          || ','
-         || cur_rec.CUSTOMER_NUMBER);
+         || cur_rec.column5);
    END LOOP;
 
    UTL_FILE.FCLOSE (v_file);
