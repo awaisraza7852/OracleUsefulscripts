81b69989541793ad253ff5ef166ad30e5a8e5ce7
awaisraza7852 <51000185+awaisraza7852@users.noreply.github.com>
2023-08-25 11:32:59 +0500

    Create Locked_objects.sql

---
+++ Locked_objects.sql
@@ -0,0 +1,51 @@
+
+--find locked objects
+
+SELECT B.Owner, B.Object_Name, A.Oracle_Username, A.OS_User_Name  
+FROM V$Locked_Object A, All_Objects B
+WHERE A.Object_ID = B.Object_ID;
+
+
+-- 
+
+
+select
+   c.owner,
+   c.object_name,
+   c.object_type,
+   b.sid,
+   b.serial#,
+   b.status,
+   b.osuser,
+   b.machine
+from
+   v$locked_object a ,
+   v$session b,
+   dba_objects c
+where
+   b.sid = a.session_id
+and a.object_id = c.object_id;
+
+
+
+
+--KILL session
+
+ALTER SYSTEM KILL SESSION '<sid, serial#>'
+select
+   c.owner,
+   c.object_name,
+   c.object_type,
+   b.sid,
+   b.serial#,
+   b.status,
+   b.osuser,
+   b.machine
+from
+   v$locked_object a ,
+   v$session b,
+   dba_objects c
+where
+   b.sid = a.session_id
+and
+   a.object_id = c.object_id;
